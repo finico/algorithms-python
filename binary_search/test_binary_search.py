cfaade6a7f298d869263954e9375098f13cbf8ae
finico <finico.a@gmail.com>
2018-08-28 13:43:53 +0300

    Binary search and configure CI

---
+++ binary_search/test_binary_search.py
@@ -0,0 +1,22 @@
+import unittest
+from random import randint
+
+from big_o import big_o, complexities
+
+from binary_search import binary_search
+
+
+class TestBinarySearch(unittest.TestCase):
+    def test_find(self):
+        self.assertEqual(binary_search([1, 3, 5, 7, 9], 3), 1)
+
+    def test_not_find(self):
+        self.assertEqual(binary_search([1, 2, 3, 4, 5], 6), None)
+
+    def test_complexity(self):
+        best, _ = big_o(
+            lambda arr: binary_search(arr, arr[randint(0, len(arr) - 1)]),
+            lambda x: list(range(x)),
+            n_repeats=1000
+        )
+        self.assertIsInstance(best, complexities.Logarithmic)
