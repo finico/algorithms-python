c33bb2035df80e76ef374437f74a1e56502fb8b6
finico <finico.a@gmail.com>
2018-08-30 18:24:32 +0300

    Improve impl of selection sort

--- selection_sort/__init__.py
+++ selection_sort/__init__.py
@@ -1,20 +1,15 @@
-def find_smallest(arr):
-    smallest_index = 0
-    smallest = arr[smallest_index]
-
-    for i in range(1, len(arr)):
-        if arr[i] < smallest:
-            smallest_index = i
-            smallest = arr[i]
-
-    return smallest_index
-
-
 def selection_sort(arr):
-    res = []
+    res = arr.copy()
 
-    for _ in range(len(arr)):
-        smallest = find_smallest(arr)
-        res.append(arr.pop(smallest))
+    for i in range(len(res) - 1):
+        least = i
+
+        for j in range(least + 1, len(res)):
+            if res[j] < res[least]:
+                least = j
+
+        temp = res[i]
+        res[i] = res[least]
+        res[least] = temp
 
     return res
